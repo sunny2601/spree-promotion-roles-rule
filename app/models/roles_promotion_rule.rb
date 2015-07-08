f92e6b65d07ff24484c6573e6f87f49f5dde6078
sunny2601 <sunnysarrot@gmail.com>
2015-07-08 16:04:46 +0530

    Update roles_promotion_rule.rb

--- app/models/roles_promotion_rule.rb
+++ app/models/roles_promotion_rule.rb
@@ -6,7 +6,7 @@
 
 
 class RolesPromotionRule < Spree::PromotionRule
-  attr_accessible :role_ids_string, :role_list
+  #attr_accessible :role_ids_string, :role_list
 
   belongs_to :role, :class_name => '::Spree::Role'
   has_and_belongs_to_many :roles, :class_name => '::Spree::Role',
